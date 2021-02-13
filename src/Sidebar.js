b93dcffa443b4175d1456f5cb6aa5e49e0c2ab03
Jerome Konieczka <jkonieczka10@gmail.com>
2021-02-13 04:32:06 -0500

    added hover effect and temp links to bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,45 @@
 import React from "react";
 import "./Sidebar.css";
+import {barData} from "./barData";
 
 function Sidebar() {
   return (
-    <div className="sidebar">
+    <div className="Sidebar">
 
-        Sidebar
-        
+      <ul className = "column">
+    {barData.map((val, key)=> {
+
+
+
+          return(
+
+              <li key={key}
+              className="button"
+              id ={window.location.pathname == val.link ? "clicked" : " "}
+              onClick={()=> {window.location.pathname = val.link;}}
+
+
+
+
+              >
+
+                <div>
+                  {val.name}
+                </div>
+
+
+              </li>
+
+            );
+
+
+
+
+    })}
+    </ul>
+
+
+
     </div>
   );
 }
---
+++ src/barData.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+
+export const barData = [
+
+{
+
+  name: "Friends",
+
+  link: "/placeholderLink1"
+
+},
+
+{
+
+  name: "Profile",
+
+  link: "/placeholderLink2"
+
+
+
+},
+
+
+
+{
+
+
+  name: "Chats",
+
+
+  link: "/placeholderLink3"
+
+
+}
+
+
+
+
+
+
+]
