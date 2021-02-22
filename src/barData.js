93f29041fede89ec87595bc4d1731b6c8ecb73d3
Jerome Konieczka <jkonieczka10@gmail.com>
2021-02-21 20:11:10 -0500

    Cleaned up the sidebar aesthetics and added icons

--- src/barData.js
+++ src/barData.js
@@ -1,8 +1,13 @@
 import React from "react";
+import ChatIcon from '@material-ui/icons/Chat';
+import PersonIcon from '@material-ui/icons/Person';
+import PeopleIcon from '@material-ui/icons/People';
 
 export const barData = [
   {
     name: "Friends",
+
+    image: <PeopleIcon/>,
 
     link: "/placeholderLink1",
   },
@@ -10,11 +15,15 @@ export const barData = [
   {
     name: "Profile",
 
+    image: <PersonIcon/>,
+
     link: "/placeholderLink2",
   },
 
   {
     name: "Chats",
+
+    image: <ChatIcon/>,
 
     link: "/placeholderLink3",
   },
