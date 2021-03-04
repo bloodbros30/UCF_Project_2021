67a722ea16b18de40ef5c6e75c2a5b3f9f3d21d1
Steve Montes <Stevemontes2@gmail.com>
2021-03-04 13:51:48 -0500

    SettingsBeing Added

--- src/barData.js
+++ src/barData.js
@@ -2,6 +2,7 @@ import React from "react";
 import ChatIcon from '@material-ui/icons/Chat';
 import PersonIcon from '@material-ui/icons/Person';
 import PeopleIcon from '@material-ui/icons/People';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 export const barData = [
   {
@@ -27,4 +28,12 @@ export const barData = [
 
     link: "/placeholderLink3",
   },
+
+  {
+    name: "Settings",
+
+    image: <SettingsIcon/>,
+
+    link: "placeholderLink4",
+  }
 ];
