a4c1873f2096d1feb96db9829ad6e7bc159f1e95
Steve Montes <Stevemontes2@gmail.com>
2021-03-07 04:09:34 -0500

    Adding IconButton API from @material-ui

---
+++ src/IconButtons.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+/**
+ * Import the icons from @material-ui/icons here
+ * 
+ * this creates and allows us to use the IconButton API from react
+ *  which as implied allows us to use icons as buttons
+ */
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+}));
+
+export default function IconButtons(){
+    const classes = useStyles();
+
+    return(
+        <div className={classes.root}>
+            <IconButton>
+
+            </IconButton>
+        </div>
+    );
+}
