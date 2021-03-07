3cbb64bb6eba7a649da6d56df2d74e7c445a9f48
Steve Montes <Stevemontes2@gmail.com>
2021-03-07 04:18:20 -0500

    Adding IconButton

--- src/IconButtons.js
+++ src/IconButtons.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 /**
  * Import the icons from @material-ui/icons here
  * 
