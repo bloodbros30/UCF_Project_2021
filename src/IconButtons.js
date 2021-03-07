66f4a2d4094724e15a6d0bf4316de36d94714a30
Steve Montes <Stevemontes2@gmail.com>
2021-03-07 04:25:31 -0500

    Adding TextButtons

--- src/IconButtons.js
+++ src/IconButtons.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
