f15b41b7652c11fb8166eda22a6778748b599c0e
unknown <huntercheung3@gmail.com>
2021-03-07 23:56:53 -0500

    added ability to edit user info

--- src/UserPage.js
+++ src/UserPage.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
 import { useContext } from 'react';
 import { UserContext } from "./UserProvider";
-
+import { auth, fs } from "./fire";
 
 import "./App.css";
 
@@ -11,8 +10,8 @@ function UserPage() {
   const user = useContext(UserContext);
 
  if (!user) return <></>;
-  
- 
+
+
   console.log(user);
     return(
       <div className = "userCard">
@@ -22,28 +21,70 @@ function UserPage() {
 
         <div className = "usercardInfo">
           <p >
-          <strong>Name:</strong> {user.Name}
+          <strong>Name:</strong> {user.name}
           </p>
 
         </div>
 
         <div className = 'row mt'>
           <div className = 'col s12 m6 l6 x l6'>
-            <p>
-              <strong>Name:</strong> {user.Name}
-            </p>
-            <p>
-              <strong>Age:</strong> {user.Age}
-            </p>
-            <p>
-              <strong>Interests:</strong> {user.Interests}
-            </p>
-            <p>
-              <strong>Address:</strong> {user.Address}
-            </p>
-            <p>
-              <strong>Username:</strong> {user.Username}
-            </p>
+
+              <strong>Name:</strong> {user.name}
+              <form>
+                <label>
+                  Name:
+                  <input type="text" name="name" />
+                  </label>
+                  <input type="submit" value="Submit" onChange={(e) => fs.collection('Users').doc(user.uid).update({
+                    "name" : e.target.value
+                  })} />
+              </form>
+
+
+              <strong>Age:</strong> {user.age}
+              <form>
+                <label>
+                  Age:
+                  <input type="text" age="age" />
+                  </label>
+                  <input type="submit" value="Submit" onChange={(e) => fs.collection('Users').doc(user.uid).update({
+                    "age" : e.target.value
+                  })} />
+              </form>
+
+              <strong>Interests:</strong> {user.interests}
+              <form>
+                <label>
+                  Age:
+                  <input type="text" age="age" />
+                  </label>
+                  <input type="submit" value="Submit" onChange={(e) => fs.collection('Users').doc(user.uid).update({
+                    "interests" : e.target.value
+                  })} />
+              </form>
+
+              <strong>Address:</strong> {user.address}
+              <form>
+                <label>
+                  Address:
+                  <input type="text" address="address" />
+                  </label>
+                  <input type="submit" value="Submit" onChange={(e) => fs.collection('Users').doc(user.uid).update({
+                    "address" : e.target.value
+                  })} />
+              </form>
+
+              <strong>Username:</strong> {user.username}
+              <form>
+                <label>
+                  Username:
+                  <input type="text" username="username" />
+                  </label>
+                  <input type="submit" value="Submit" onChange={(e) => fs.collection('Users').doc(user.uid).update({
+                    "username" : e.target.value
+                  })} />
+              </form>
+
           </div>
         </div>
       </div>
