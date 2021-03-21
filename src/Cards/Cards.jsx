e7f16f12fcb92b5cce7d206ec5c8b59b9c7af301
Steve Montes <Stevemontes2@gmail.com>
2021-03-21 13:09:08 -0400

    adding Cards.jsx for usage

---
+++ src/Cards/Cards.jsx
@@ -0,0 +1,31 @@
+import React,{Component} from 'react';
+import Card from './CardsUI';
+
+//Change the photo to anything you want I just had this on hand
+
+import img1 from '../Assest/gundam1.jpg';
+import img2 from '../Assest/gundam2.jpg';
+import img3 from '../Assest/gundam3.jpg';
+
+
+class Cards extends Component {
+    render(){
+        return(
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="row">
+                    <div className="col-md-4">
+                        <Card imgsrc={img1}title="Console"/>
+                    </div>
+                    <div className="col-md-4">
+                        <Card imgsrc={img2}title="Playgroud"/>
+                    </div>
+                    <div className="col-md-4">
+                        <Card imgsrc={img3}title="Adventure"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Cards;
