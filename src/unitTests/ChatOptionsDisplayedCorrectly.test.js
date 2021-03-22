1e459cd0453d0be2914de9c098bb9f5749c54272
Jerome Konieczka <jkonieczka10@gmail.com>
2021-03-22 00:06:38 -0400

    Added unit test for chat options/ browse screen and for date creation on message items

---
+++ src/unitTests/ChatOptionsDisplayedCorrectly.test.js
@@ -0,0 +1,16 @@
+//makes sure the new chats are displayed correctly in options
+
+
+import BrowseChats from './BrowseChats'
+
+test('tests the display of chatOptions', () => {
+
+
+
+  const chats = ["chat1", "chat2"];
+
+
+    expect(chats.map()).toHaveBeenCalled();
+    expect(useEffect()).toHaveBeenCalled();
+
+  });
