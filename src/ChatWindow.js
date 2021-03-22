4b891dfa276736c15c49e7d316db26d88e6f6ef8
alerubbio <rubio.alejandro7@knights.ucf.edu>
2021-03-21 22:21:02 -0400

    fixed chatWindow
    chat window references correct chats

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -15,28 +15,13 @@ function ChatWindow({ selectedChat }) {
   const user = useContext(UserContext);
   const [formValue, setFormValue] = useState("");
   const [messages, setMessages] = useState([]);
+  const unsub = fs.collection("Chats").doc(selectedChat).collection("messages");
 
-  useEffect(() => {
-    // eventually change to fs.collection("users").doc(selectedChat).collection("messages")
-
-    const unsub = fs
-      .collection("messages")
-      .orderBy("sentAt")
-      .onSnapshot((querySnapshot) => {
-        setMessages(
-          querySnapshot.docs.map((doc) => {
-            return { ...doc.data(), id: doc.id };
-          })
-        );
-      });
-
-    return () => unsub();
-  }, []);
 
   async function sendMessage(e) {
     e.preventDefault();
     if (!formValue) return;
-    await fs.collection("messages").add({
+    await unsub.add({
       messageText: formValue,
       sentAt: firebase.firestore.FieldValue.serverTimestamp(),
       userID: user.uid,
@@ -45,110 +30,17 @@ function ChatWindow({ selectedChat }) {
   }
 
   function createDate(secs ){
-
-
-    var time =  new Date(1970, 0, 1);
+    var time = new Date(1970, 0, 1);
     time.setSeconds(secs);
     return time.toString();
-
-
-
-
   }
 
-
-
-  /*
-
-  const messagesRef = fs.collection("messages");
-  var collection;
-  var gotMessagesAlready = false;
-
-  //const query = collection.orderBy('sentAt').limit(25);
-  //const [messages] = useCollectionData(query, {idField: 'id'});
-  const [formValue, setFormValue] = useState("");
-  const user = firebase.auth().currentUser;
-  var used = false;
-  var btn = document.getElementById("sendBtn");
-
-  //btn.disabled = formValue == '';
-  //console.log(messages[0]); //this is broken
-
-  if (!user) return <></>;
-  var curUserId = user.id;
-  console.log(messagesRef);
-  //setInterval(fillMessages(), 5000);
-
-  console.log("HERE IS COLLECTION");
-
-  //fillMessages();
-
-  /*function populateMessage(collection){
-  collection.forEach(doc => {
-    console.log(doc.id, '=>', doc.data());
-  });
-
-}*/
-  /*
-  async function fillMessages() {
-    console.log(user);
-    if (!gotMessagesAlready) {
-      //do all the shit here
-
-      //get the message from database
-      collection = await messagesRef.orderBy("sentAt").limit(25).get();
-      if (collection.empty) {
-        console.log("information not obtained successfully");
-      } else {
-        var localuid = user.L;
-
-        var list = document.getElementById("chatLog");
-        list.innerHTML = "";
-        collection.forEach((doc) => {
-          console.log(doc.id, "=>", doc.data());
-
-          var aMessage = document.createElement("li");
-          aMessage.innerHTML = doc.data().messageText;
-
-          console.log("got inside first if");
-          if (localuid == doc.data().userID) {
-            aMessage.classList.add("chatMessageME");
-            console.log(curUserId);
-          } else {
-            console.log(curUserId);
-            console.log("failed");
-            aMessage.classList.add("chatMessageOther");
-          }
-
-          list.appendChild(aMessage);
-        });
-
-        //returnFunc(collection);
-      }
-    }
-
-    gotMessagesAlready = true;
-  }
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    await messagesRef.add({
-      messageText: formValue,
-      sentAt: firebase.firestore.FieldValue.serverTimestamp(),
-      test: 0,
-      userID: user.uid,
-    });
-
-    setFormValue("");
-    fillMessages();
-    //dummy.current.scrollIntoView({behavior: 'smooth'});
-  };
-*/
   return (
     <>
       <div className="ChatWindow">
-        <div className="chat-title">{selectedChat}</div>
+        <div className="chat-title">
+          {selectedChat}
+        </div>
 
         <form className="text-form">
           <textarea
