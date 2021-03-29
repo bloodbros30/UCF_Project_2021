00c63ff7e9e18d09a40bc19e135a2dea33309cc9
Nelson <nelsondavidreina@gmail.com>
2021-03-28 20:24:11 -0400

    Fixed bug

--- src/ChatPage.js
+++ src/ChatPage.js
@@ -4,11 +4,16 @@ import BrowseChats from "./BrowseChats.js";
 import ChatSearch from "./ChatSearch.js";
 import Card from './Cards/Cards'
 import './ChatPage.css'
+import {useContext} from "react"
+import { UserContext } from "./UserProvider";
 
 export default function ChatPage() {
+  const user = useContext(UserContext);
+
   return (
     <div className='ChatPage'>
-      <UserChatList/>
+      {user.chats && (
+      <UserChatList/>)}
       <BrowseChats/>
 
       <ChatSearch/>
